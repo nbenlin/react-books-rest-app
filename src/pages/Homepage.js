d4ac450b6dd50aaa66a2cce180139dc368bb24ef
nbenlin <nihatbenli.nb@gmail.com>
2022-01-20 18:42:16 +0100

    Create Homepage component

---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import styles from "./Homepage.module.css";
+
+export const Homepage = () => {
+  return (
+    <div className={styles.homeContainer}>
+      <header className={styles.header}>
+        <img
+          className={styles.books}
+          src="https://freesvg.org/img/book-pack.png"
+          alt="books"
+        />
+        <h1>Book finder</h1>
+      </header>
+      <main></main>
+    </div>
+  );
+};
