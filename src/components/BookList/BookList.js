3793d89ce09aaa7030c5ff4b61fea3f341ef8d0f
nbenlin <nihatbenli.nb@gmail.com>
2022-01-21 17:32:15 +0100

    Modify props and style

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -3,12 +3,21 @@ import styles from "./BookList.module.css";
 
 const BookList = ({ books }) => {
   console.log(books.map((item) => console.log(item)));
+
   return (
-    <div className={styles.listContainer}>
-      {books.map((book) => (
-        <Card key={book.id} book={book} />
-      ))}
-    </div>
+    <>
+      <div className={styles.booksMenu}>
+        <div className={styles.daily}></div>
+        <div className={styles.newReleases}></div>
+        <div className={styles.bestsellers}></div>
+        <div className={styles.topAuthors}></div>
+      </div>
+      <div className={styles.listContainer}>
+        {books.map((book) => (
+          <Card key={book.id} book={book} />
+        ))}
+      </div>
+    </>
   );
 };
 export default BookList;
