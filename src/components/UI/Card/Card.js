faa3ee36241abdb4b29a70836115f13ff3325185
nbenlin <nihatbenli.nb@gmail.com>
2022-01-21 13:11:47 +0100

    Add properties to Card compontn

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,18 +1,31 @@
 import styles from "./Card.module.css";
 
 const Card = ({ book }) => {
+  let titleMaxLength = 20;
+  const titleWithEllipsis =
+    book.volumeInfo.title.length > titleMaxLength
+      ? book.volumeInfo.title.substring(0, titleMaxLength) + "..."
+      : book.volumeInfo.title;
+
+  const authors = book.volumeInfo.authors.toString();
   return (
     <div className={styles.card}>
       <div className={styles.cardHeader}>
-        <img
-          className={styles.image}
-          src={book.volumeInfo.imageLinks.thumbnail}
-          alt="book"
-        />
+        <a
+          href={book.volumeInfo.canonicalVolumeLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className={styles.image}
+            src={book.volumeInfo.imageLinks.thumbnail}
+            alt="book"
+          />
+        </a>
       </div>
       <div className={styles.cardBody}>
-        <p className={styles.title}>Book title</p>
-        <p className={styles.author}>Book Author</p>
+        <p className={styles.title}>{titleWithEllipsis}</p>
+        <p className={styles.author}>{authors}</p>
       </div>
     </div>
   );
