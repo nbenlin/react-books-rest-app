05bad12cf65addce2e1b19cecdab29cd543a6ed3
nbenlin <nihatbenli.nb@gmail.com>
2022-01-22 12:42:29 +0100

    Add modify to BookList comp

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -2,20 +2,19 @@ import Card from "../UI/Card/Card";
 import styles from "./BookList.module.css";
 
 const BookList = ({ books }) => {
-  console.log(books.map((item) => console.log(item)));
+  // console.log(books.map((item) => console.log(item)));
 
   return (
     <>
       <div className={styles.booksMenu}>
-        <div className={styles.daily}></div>
-        <div className={styles.newReleases}></div>
-        <div className={styles.bestsellers}></div>
-        <div className={styles.topAuthors}></div>
+        <div className={styles.daily}>Daily Top</div>
+        <div className={styles.newReleases}>New releases</div>
+        <div className={styles.bestsellers}>Bestsellers</div>
+        <div className={styles.topAuthors}>Top authors</div>
       </div>
       <div className={styles.listContainer}>
-        {books.map((book) => (
-          <Card key={book.id} book={book} />
-        ))}
+        {books !== undefined &&
+          books.map((book) => <Card key={book.id} book={book} />)}
       </div>
     </>
   );
