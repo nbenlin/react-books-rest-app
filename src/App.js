c01f6f964f0142a5e140bb349bbecaa4d2fc2f52
nbenlin <nihatbenli.nb@gmail.com>
2022-01-24 14:12:11 +0100

    Modified App.js file after creating the .env file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Homepage from "./pages/Homepage";
 
 const App = () => {
   const [data, setData] = useState([]);
-
-  const KEY = "AIzaSyDuPfWGUeEexwNBIlhsZHbaU6WpstVYHaI";
+  const [mainMenuId, setMainMenuId] = useState("");
+  const [sidebarId, setSidebarId] = useState("");
 
   useEffect(() => {
-    fetchBooks();
-  }, []);
+    fetchDataHandler();
+  }, [mainMenuId, sidebarId]);
 
-  const fetchBooks = () => {
+  const menuClickHandler = (menuName) => {
+    setMainMenuId(menuName);
+  };
+
+  const sidebarClickHandler = (sidebarName) => {
+    setSidebarId(sidebarName);
+  };
+
+  const fetchDataHandler = (textToFilter) => {
+    const filterText = textToFilter;
+    if (mainMenuId.length > 0) {
+      if (mainMenuId === "daily") {
+        fetchSelectedBooks("love");
+      }
+      if (mainMenuId === "newReleases") {
+        fetchSelectedBooks("feminism");
+      }
+      if (mainMenuId === "topAuthors") {
+        fetchSelectedBooks("drama");
+      }
+      if (filterText === "bestsellers") {
+        fetchSelectedBooks("inspirational");
+      }
+    }
+    if (sidebarId) {
+      fetchSelectedBooks(sidebarId);
+    }
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=${KEY}`
+      `https://www.googleapis.com/books/v1/volumes?q=${filterText}&orderBy=newest&key=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data.items))
+      .catch((err) => console.log("Error: ", err));
+  };
+
+  const fetchSelectedBooks = (subject) => {
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=subject:${subject}&startIndex=54&maxResults=6`
     )
       .then((res) => res.json())
       .then((data) => setData(data.items))
@@ -22,7 +57,12 @@ const App = () => {
 
   return (
     <>
-      <Homepage books={data} />
+      <Homepage
+        filterHandler={fetchDataHandler}
+        books={data}
+        menuClickHandler={menuClickHandler}
+        sidebarClickHandler={sidebarClickHandler}
+      />
     </>
   );
 };
